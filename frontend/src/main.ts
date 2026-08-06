bc3adcc4e7de23b16810f7a40f2c8e557dd2ecc9
yourdevharsh <yourdevharsh@gmail.com>
2026-08-06 18:38:23 +0530

    Added input fields for starting building server.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,58 @@
-import './style.css'
+import "./style.css";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const userPromptEl = document.querySelector(
+    "#userPrompt",
+  ) as HTMLTextAreaElement | null;
+  const fileInputsEl = document.querySelector(
+    "#fileInputs",
+  ) as HTMLInputElement | null;
+  const submitButtonEl = document.querySelector("#submitButton");
+  const resultEl = document.querySelector(
+    "#result",
+  ) as HTMLHeadingElement | null;
+
+  submitButtonEl?.addEventListener("click", () => {
+    const userPrompt = userPromptEl?.value;
+    if (!userPrompt) {
+      if (resultEl) {
+        resultEl.innerHTML = "No Instruction";
+      }
+      return;
+    }
+
+    const files = fileInputsEl?.files;
+    if (!files) {
+      if (resultEl) {
+        resultEl.innerHTML = "No Instruction";
+      }
+      return;
+    }
+
+    processRequest(files, userPrompt);
+  });
+});
+
+async function processRequest(
+  files: FileList,
+  userPrompt: string,
+): Promise<void> {
+  const formData = new FormData();
+
+  Array.from(files).forEach((file) => {
+    formData.append("files", file);
+  });
+
+  formData.append("userPrompt", userPrompt);
+
+  try {
+    const response = await fetch("http://localhost:8000/api/v1/process-doc", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+  } catch (error) {}
+}
